36b43a91000cf952fabee94cee3c12442af1481c
Emmanuel-okello-awany <2300811768@std.kyu.ac.ug>
2025-09-05 04:05:41 -0700

    created the foundation base for the web based projects and started all the projects to be worked on

---
+++ WEB-BASED/Lost_Item_System_for_Schools/Lost_Item_System_for_Schools/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Lost_Item_System_for_Schools project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lost_Item_System_for_Schools.settings')
+
+application = get_asgi_application()
